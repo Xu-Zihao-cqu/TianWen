0483d6c4282124fcb45d79b1b96088da862d6c39
Xu <2183500965@qq.com>
2026-09-19 21:28:52 +0800

    inte-car

--- src/data/works/resources.js
+++ src/data/works/resources.js
@@ -32,4 +32,35 @@ export const resourcesWorks = [
     featured: false,
     createdAt: '2025-06-01',
   },
+
+  {
+    id: 'Intellignet Car',
+    category: 'resources',
+    title: { zh: '智能小车', en: 'Intellignet Car' },
+    coverImage: '/images/works/inte-car.svg',
+    description: {
+      short: {
+        zh: '支持循迹、超声波避障、蓝牙的智能小车项目 。',
+        en: 'Smart car project supporting line tracking, ultrasonic obstacle avoidance, and Bluetooth connectivity.',
+      },
+      full: {
+        zh: '## 内容简介\n\n该项目使用IRT9909实现循迹功能、使用JDY-31实现蓝牙通信、使用HCSR04实现超声波避障,不同功能依靠用户按键切换。',
+        en: '## Overview\n\nThis project utilizes the IRT9909 for line-following, the JDY-31 for Bluetooth communication, and the HCSR04 for ultrasonic obstacle avoidance, with different functions toggled via user buttons.',
+      },
+    },
+    tags: ['On-school Honework',  '2026'],
+    files: [
+      {
+        name: 'inte_car.rar',
+        type: 'rar',
+        size: '800 KB',
+        url: '/files/resources/inte_car.rar',
+        previewable: true,
+      },
+    ],
+    externalLinks: [],
+    featured: false,
+    createdAt: '2025-09-19',
+  }
+
 ];
